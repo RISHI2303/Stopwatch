d2781b7eb340c9804c7197bf7a3172e79ff81fb2
Rishi Goyal <rishigoyal230@gmail.com>
2022-04-02 04:03:32 +0530

    added lap-reset

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 let hrs = document.getElementById("hrs");
 let min = document.getElementById("min");
 let sec = document.getElementById("sec");
-let startStop = document.getElementById("start/stop");
-let lapReset = document.getElementById("lap/reset");
+let startStop = document.getElementById("start-stop");
+let lapReset = document.getElementById("lap-reset");
+let lapList = document.querySelector(".lap-container");
 let lapCounter = 0;
 let time = 0;
 let isRunning = false;
@@ -13,6 +14,14 @@ function init() {
 	startStop.style.color = "green";
     
     lapReset.style.backgroundColor = "#b3d6ce";
+    hrs.innerHTML = "00";
+    min.innerHTML = "00";
+    sec.innerHTML = "00";
+    lapReset.innerHTML = "Reset";
+    startStop.innerHTML = "Start";
+    time = 0;
+    isRunning = false;
+    lapCounter = 0;
 }
 
 init();
@@ -23,14 +32,27 @@ startStop.addEventListener("click", function () {
         startStop.innerHTML = "Stop";
         startStop.style.backgroundColor = "#f4cccc";
         startStop.style.color = "red";
+        lapReset.innerHTML = "Lap";
         timerId = setInterval(function () {
             time++;
             let secs = time % 60;
             let mins = parseInt(time / 60);
             let hrs = parseInt(mins / 60);
-            sec.innerHTML = secs;
-            min.innerHTML = mins;
-            hrs.innerHTML = hrs;
+            if(secs < 10){
+                sec.innerHTML = "0" + secs;
+            } else {
+                sec.innerHTML = secs;
+            }
+            if(mins < 10){
+                min.innerHTML = "0" + mins;
+            } else {
+                min.innerHTML = mins;
+            }
+            if (hrs < 10) {
+                hrs.innerHTML = "0" + hrs;
+            } else {
+                hrs.innerHTML = hrs;
+            }
         }, 1000);
     }
 
@@ -40,5 +62,34 @@ startStop.addEventListener("click", function () {
         clearInterval(timerId);
         startStop.style.backgroundColor = "#b6d7a8";
         startStop.style.color = "green";
+        lapReset.innerHTML = "Reset";
     }
- });
+});
+
+function createLap() { 
+    let div = document.createElement("div");
+    div.setAttribute("id", "timeDiv");
+    let divider = document.createElement("hr");
+    let lapNum = document.createElement("span");
+    lapNum.innerHTML = "Lap " + lapCounter;
+    lapCounter++;
+
+    let timeStamp = document.createElement("span");
+    timeStamp.innerHTML = `${hrs.innerHTML} : ${min.innerHTML} : ${sec.innerHTML}`;
+    div.appendChild(lapNum);
+    div.appendChild(timeStamp);
+    lapList.appendChild(divider);
+    lapList.appendChild(div);
+}
+ 
+lapReset.addEventListener("click", function () { 
+    if (isRunning == true) { 
+        createLap();
+    }
+
+    else {
+        lapList.innerHTML = "";
+        lapReset.innerHTML = "Reset";
+        init();
+    }
+});
